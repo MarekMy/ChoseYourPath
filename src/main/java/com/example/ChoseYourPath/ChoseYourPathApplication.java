bd035b2758cceb312ced87db49386efb5b1864de
MarekMy <marek.my@gmail.com>
2022-01-14 23:15:22 +0100

    First Commit

---
+++ src/main/java/com/example/ChoseYourPath/ChoseYourPathApplication.java
@@ -0,0 +1,13 @@
+package com.example.ChoseYourPath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoseYourPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoseYourPathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ChoseYourPath/ChoseYourPathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChoseYourPath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoseYourPathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
